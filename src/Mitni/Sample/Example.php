5682d10d1311eb36f4b782885d1ea7574f17e4fb
Nick Mitchell <nick@superlogical.org>
2016-02-12 21:12:04 +1300

    first commit

---
+++ src/Mitni/Sample/Example.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Mitni\Sample;
+
+/**
+ * A terribly useful class
+ *
+ * 
+ */
+class Example
+{
+    public function sayHello()
+    {
+        return 'Hallo!';
+    }
+    
+    public function sayGoodbye(){
+        return 'Goodbye';
+    }
+}
